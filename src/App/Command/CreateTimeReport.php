f20b870ab3f230a548a7525f0446bd4f8626f8c3
Konstantin Sumarokov <k@chidev.ru>
2024-11-09 11:39:42 +0500

    Команда с параметрами в объект с датой отчета

---
+++ src/App/Command/CreateTimeReport.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Command;
+
+use DateInterval;
+use DateTime;
+use Exception;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Webmozart\Assert\Assert;
+
+#[AsCommand(name: 'app:create-timereport', aliases: ['tr'])]
+class CreateTimeReport extends Command
+{
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('month', InputArgument::OPTIONAL, 'Месяц: 1-12 или prev или cur', 'prev')
+            ->addArgument('year', InputArgument::OPTIONAL, 'Год', date('Y'))
+            ->setDescription('Генерирует отчет за указанный период: tr | tr prev | tr cur | tr 6 | tr 6 2020');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+
+        try {
+            $argMonth = $input->getArgument('month');
+            $argYear = $input->getArgument('year');
+
+            if ($argMonth === 'prev') {
+                $reportDT = (new DateTime())->sub(new DateInterval('P1M'));
+            } elseif ($argMonth === 'cur') {
+                $reportDT = new DateTime();
+            } else {
+                $reportYear = (int)$argYear > 0 ? (int)$argYear : date('Y');
+                Assert::range($reportYear, 2000, 3000, "Укажите корректный год");
+                $reportMonth = (int)$argMonth >= 0 ? (int)$argMonth : date('n');
+                Assert::range($reportMonth, 1, 12, "Укажите корректный месяц");
+                $reportDT = DateTime::createFromFormat('Y-m', "$reportYear-$reportMonth");
+            }
+            $output->writeln("Запрошен отчёт за {$reportDT->format('Y')} год и {$reportDT->format('n')} месяц");
+        } catch (Exception $e) {
+            $output->writeln("Некорректная команда: {$e->getMessage()}");
+            return Command::INVALID;
+        }
+
+        //@todo call and out
+
+        return Command::SUCCESS;
+    }
+
+}
